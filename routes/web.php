aff342d15c1899b817dd457e72cb4e6617240420
salsabila295 <70619506+salsabila295@users.noreply.github.com>
2021-01-15 16:00:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('home');
 });
 
-// Closure
-Route::get('/profil', function(){
-    return "GET READY? SHOWTIME!";
-})->name('profil');
-
-// Route name
-Route::get("/tesname", function(){
-    return route('profil');
+Route::get('/about', function(){
+    return view('about');
 });
-
-// Parameter
-Route::get('/profil/{id}', function(){
-    return $id;
-});
-
